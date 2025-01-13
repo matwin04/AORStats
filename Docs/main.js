33656d884570302472bf6ddf1592529190a72fba
frostie <mattwiner04@gmail.com>
2025-01-13 13:16:13 -0800

    3

---
+++ Docs/main.js
@@ -0,0 +1,24 @@
+const jsonFile = './leaderboard.json';
+fetch(jsonFile)
+    .then(response => response.json())
+    .then(data=>{
+        const tableBody = document.querySelector("#leaderboard tbody");
+        tableBody.innerHTML = '';
+        data.forEach(player => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${player.id}</td>
+                <td>${player.name}</td>
+                <td>${player.region}</td>
+                <td>${player.track}</td>
+                <td>${player.class}</td>
+                <td>${player.car}</td>
+                <td>${player.time}</td>
+            `;
+            tableBody.appendChild(row);
+            
+        });
+    })
+    .catch(error=>{
+        console.error('error loading json',error);
+    });
