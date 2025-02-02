9705195c56a7f8202359bc762fcb1a58d9b1f799
frostie <mattwiner04@gmail.com>
2025-02-02 11:06:50 -0800

    5. Plays test.mp3

---
+++ PSPBuild/CMakeFiles/PSPlayer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PSPlayer.dir/main.cpp.obj"
+  "CMakeFiles/PSPlayer.dir/main.cpp.obj.d"
+  "PSPlayer"
+  "PSPlayer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PSPlayer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <pspkernel.h>
+#include <pspaudio.h>
+#include <pspaudiolib.h>
+#include <SDL.h>
+#include <SDL_mixer.h>
+void setupPSP() {
+    pspDebugScreenInit();
+    pspAudioInit();
+}
+int main(int argc,char *argv[]) {
+    setupPSP();
+    SDL_Init(SDL_INIT_AUDIO);
+    Mix_OpenAudio(44100,MIX_DEFAULT_FORMAT,2,2048);
+
+    Mix_Music *music = Mix_LoadMUS("test.mp3");
+    if (!music) {
+        pspDebugScreenPrintf("Error: Failed to load music: %\n",Mix_GetError());
+        return 1;
+    }
+    Mix_PlayMusic(music,-1);
+    pspDebugScreenPrintf("Playing TEST.mp3 Press [X] to exit\n");
+    
+    SDL_Event event;
+    int running = 1;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.sym ==SDLK_x) {
+                    running = 0;
+                }
+            }
+        }
+        SDL_Delay(100);
+    }
+    Mix_FreeMusic(music);
+    Mix_CloseAudio();
+    SDL_Quit();
+    pspAudioEnd();
+    sceKernelExitGame();
+    return 0;
+}
